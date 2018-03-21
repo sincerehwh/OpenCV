4dc73db716fe2b66f79a976a65a3dc147c9981d6
louis <louishwh@gmail.com>
2018-03-21 15:49:13 +0800

    try to use opencv

---
+++ CV/CV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CV
+//
+//  Created by louis on 2018/3/21.
+//  Copyright © 2018年 JuFeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
